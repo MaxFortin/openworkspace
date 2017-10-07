731ae3f31b57d2a4a41d996a36ad1e99506e9303
Max Fortin <maximusprime365@gmail.com>
2017-10-07 18:54:30 -0400

    First Commit

---
+++ main/java/openworkspace/Main.java
@@ -0,0 +1,12 @@
+package openworkspace;
+
+import org.bytedeco.javacpp.opencv_videoio.VideoCapture;
+
+public class Main {
+
+	public static void main(String[] args) {
+		VideoCapture cap = new VideoCapture();
+
+	}
+
+}
